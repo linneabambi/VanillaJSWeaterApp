4512ae0bf7bc40f57ab533e4fb327bd3bc37741d
linneabambi <linnealm99@live.se>
2022-02-05 13:47:17 +0100

    celcius farenheit coversion and style

--- src/script.js
+++ src/script.js
@@ -24,14 +24,15 @@ function newDate(timestamp) {
 function showTemperature(response) {
   let degreesElement = document.querySelector("#degrees");
   let cityElement = document.querySelector("#current-city");
-  let temperature = Math.round(response.data.main.temp);
+
   let description = document.querySelector("#sky");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let date = document.querySelector("#day-time");
   let iconElement = document.querySelector("#icon");
+  celciusTemp = Math.round(response.data.main.temp);
 
-  degreesElement.innerHTML = temperature;
+  degreesElement.innerHTML = celciusTemp;
   cityElement.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
@@ -43,8 +44,39 @@ function showTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "cfecd485617989b8f0add91581222571";
 
-let apiKey = "cfecd485617989b8f0add91581222571";
-let city = "Mariestad";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  search(searchInput.value);
+}
+function displayFarenheit(event) {
+  event.preventDefault();
+  let farenheitTemp = (celciusTemp * 9) / 5 + 32;
+  let temp = document.querySelector("#degrees");
+  temp.innerHTML = Math.round(farenheitTemp);
+  cLink.classList.remove("active");
+  fLink.classList.add("active");
+}
+function displayCelcius(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#degrees");
+  temp.innerHTML = celciusTemp;
+  cLink.classList.add("active");
+  fLink.classList.remove("active");
+}
+let celciusTemp = null;
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", searchSubmit);
+let fLink = document.querySelector("#farenheit-link");
+fLink.addEventListener("click", displayFarenheit);
+let cLink = document.querySelector("#celcius-link");
+cLink.addEventListener("click", displayCelcius);
+
+search("New York");
