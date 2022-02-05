3365d718d6ab90908d826e3985d682bab19b1462
linneabambi <linnealm99@live.se>
2022-02-05 11:56:25 +0100

    weather icons

--- src/script.js
+++ src/script.js
@@ -25,20 +25,26 @@ function showTemperature(response) {
   let degreesElement = document.querySelector("#degrees");
   let cityElement = document.querySelector("#current-city");
   let temperature = Math.round(response.data.main.temp);
-  degreesElement.innerHTML = temperature;
-
-  cityElement.innerHTML = response.data.name;
   let description = document.querySelector("#sky");
-  description.innerHTML = response.data.weather[0].description;
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.main.humidity;
   let wind = document.querySelector("#wind");
-  wind.innerHTML = Math.round(response.data.wind.speed);
   let date = document.querySelector("#day-time");
+  let iconElement = document.querySelector("#icon");
+
+  degreesElement.innerHTML = temperature;
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
   date.innerHTML = newDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "cfecd485617989b8f0add91581222571";
-let city = "London";
+let city = "Mariestad";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);
